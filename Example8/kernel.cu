946fa7c4654f63449303bf8b4fa511d8e54f88ec
Justin Chan <justinchanuk@gmail.com>
2017-11-25 16:29:46 +0800

    Corrected Example8 kernel function - dot. Coding error.

--- Example8/kernel.cu
+++ Example8/kernel.cu
@@ -16,7 +16,7 @@ __global__ void dot(float *a, float *b, float *c) {
 
 	float temp = 0;
 	while (tid < N) {
-		temp += a[tid] + b[tid];
+		temp += a[tid] * b[tid];
 		tid += blockDim.x*gridDim.x;
 	}
 
@@ -27,7 +27,7 @@ __global__ void dot(float *a, float *b, float *c) {
 	while (i != 0) {
 		if (cacheIndex < i)
 			cache[cacheIndex] += cache[cacheIndex + i];
-	
+
 		__syncthreads();
 		i /= 2;
 	}
@@ -54,12 +54,12 @@ int main(void) {
 	}
 
 
-	HANDLE_ERROR(cudaMemcpy(dev_a, a, N*sizeof(float), cudaMemcpyHostToDevice));
-	HANDLE_ERROR(cudaMemcpy(dev_b, b, N*sizeof(float), cudaMemcpyHostToDevice));
+	HANDLE_ERROR(cudaMemcpy(dev_a, a, N * sizeof(float), cudaMemcpyHostToDevice));
+	HANDLE_ERROR(cudaMemcpy(dev_b, b, N * sizeof(float), cudaMemcpyHostToDevice));
 
 	dot <<<blocksPerGrid, threadsPerBlock >>> (dev_a, dev_b, dev_partial_c);
 
-	HANDLE_ERROR( cudaMemcpy(partial_c, dev_partial_c, blocksPerGrid*sizeof(float), cudaMemcpyDeviceToHost));
+	HANDLE_ERROR(cudaMemcpy(partial_c, dev_partial_c, blocksPerGrid * sizeof(float), cudaMemcpyDeviceToHost));
 
 
 	c = 0;
@@ -75,7 +75,7 @@ int main(void) {
 	cudaFree(dev_b);
 	cudaFree(dev_partial_c);
 
-	free(dev_a);
-	free(dev_b);
+	free(a);
+	free(b);
 	free(partial_c);
 }
